b414e6165ff9229421ab04a475f6086c045bc635
Yuxuan Zhang <kate900115@gmail.com>
2018-06-21 15:38:13 +0800

    modified:   Makefile 	new file:   main_atomic.cu 	new file:   main_barrier.cu

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all:
-	nvcc -gencode=arch=compute_60,code=sm_60 -rdc=true main.cu
+	nvcc -gencode=arch=compute_60,code=sm_60 -rdc=true main_barrier.cu -o barrier.out
+	nvcc main_atomic.cu -o atomic.out
 clean:
 	rm *.out
---
+++ main_atomic.cu
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <cooperative_groups.h>
+
+using namespace cooperative_groups;
+
+__device__ int monitor;
+__device__ int signal;
+
+__global__ void vecAdd(int m, int n, float* A, float* B, float* C ){
+	int j = blockIdx.x * blockDim.x + threadIdx.x;
+	int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int ii = threadIdx.x;
+	int jj = threadIdx.y;
+
+	int blockNum = blockDim.x * blockDim.y * blockDim.z;
+
+	int count = 0;
+
+	while (count<100){
+		count++;
+	//	printf("A[%d][%d]\n",i,j);
+
+		// barrier
+		if ((ii==0)&&(jj==0)){
+			atomicAdd(&monitor, 1);
+			if (atomicCAS(&monitor, blockNum, 0)==blockNum){
+				atomicCAS(&signal, 0, 1);
+			}
+			while(!signal);
+			atomicAdd(&monitor, 1);
+			if (atomicCAS(&monitor, blockNum, 0)==blockNum){
+				atomicCAS(&signal, 1, 0);
+			}
+			while(signal);
+		}
+
+		__syncthreads();
+
+		if ((i<m)&&(j<n)) {
+			C[i*n+j] = A[i*n+j]+B[i*n+j];
+	//		printf("A[%d][%d]=%f\n",i,j,A[i*n+j]);
+		}
+		
+
+	}
+	
+
+}
+
+int main(){
+	int m = 512;
+	int n = 256;
+	float* h_a = NULL;
+	float* h_b = NULL;
+	float* h_c = NULL;
+	float* d_a = NULL;
+	float* d_b = NULL;
+	float* d_c = NULL;
+	h_a = (float*)malloc(m*n*sizeof(float));
+	h_b = (float*)malloc(m*n*sizeof(float));
+	h_c = (float*)malloc(m*n*sizeof(float));
+
+	cudaMalloc((void**)&d_a, m*n*sizeof(float));
+	cudaMalloc((void**)&d_b, m*n*sizeof(float));
+	cudaMalloc((void**)&d_c, m*n*sizeof(float));
+
+	if ((h_a==NULL)||(h_b==NULL)||(h_c==NULL)||(d_a==NULL)||(d_b==NULL)||(d_c==NULL)){
+		printf("cannot allocate memory.\n");
+	}
+	
+	//memset(h_c,0,m*n*sizeof(float));
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			h_a[i*n+j]=i+j;
+			h_b[i*n+j]=i+j;
+			h_c[i*n+j]=0;
+			//printf("%f,%f,%f\n",h_a[i*n+j],h_b[i*n+j],h_c[i*n+j]);
+		}
+	}
+	
+	cudaMemcpy(d_a, h_a, m*n*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, h_b, m*n*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_c, h_c, m*n*sizeof(float), cudaMemcpyHostToDevice);
+	dim3 grid((n+15)/16, (m+15)/16,1);
+	dim3 block(16, 16,1);
+	vecAdd<<<grid, block>>>	(m, n, d_a, d_b, d_c);
+	cudaMemcpy(h_c, d_c, m*n*sizeof(float), cudaMemcpyDeviceToHost);
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			//printf("C[%d][%d]=%f\n",i,j,h_c[i*n+j]);
+		}
+	}	
+	return 0;	
+	
+}
